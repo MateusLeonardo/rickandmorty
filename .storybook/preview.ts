0453af64f809074e297a996a19f2a9d704a31e20
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-02-09 18:31:37 -0300

    chore(storybook): add MemoryRouter decorator and cleanup configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,17 @@
 import type { Preview } from "@storybook/react-vite";
+import React from "react";
+import { MemoryRouter } from "react-router";
+import "../src/index.css";
 
 const preview: Preview = {
+  decorators: [
+    (Story) =>
+      React.createElement(
+        MemoryRouter,
+        null,
+        React.createElement(Story, null),
+      ),
+  ],
   parameters: {
     controls: {
       matchers: {
@@ -8,7 +19,6 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-
     a11y: {
       test: "todo",
     },
