308fdaefd47d144a4e23f01fd26e032fbcad0e29
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-02-09 18:31:05 -0300

    feat(pages): implement HomePage with random character selection

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,40 @@
+import { useMemo } from "react";
+import { Link } from "react-router";
+import { useGetAllCharacters } from "../hooks/queries/characters/use-get-all-characters";
+import { ContentGrid } from "../components/ui/ContentGrid/ContentGrid";
+import { ContentGridSkeleton } from "../components/ui/ContentGrid/ContentGridSkeleton";
+import { CharacterCard } from "../components/ui/CharacterCard/CharacterCard";
+import { CharacterCardSkeleton } from "../components/ui/CharacterCard/CharacterCardSkeleton";
+
 export function HomePage() {
+  const aleatoryPage = useMemo(() => Math.floor(Math.random() * 42) + 1, []);
+  const { data: characters, isLoading } = useGetAllCharacters(aleatoryPage);
+
+  const randomCharacters = useMemo(() => {
+    if (!characters?.results?.length) return [];
+    return [...characters.results].sort(() => Math.random() - 0.5).slice(0, 10);
+  }, [characters?.results]);
+
+  if (isLoading) {
+    return (
+      <ContentGridSkeleton
+        length={10}
+        renderSkeleton={() => <CharacterCardSkeleton />}
+      />
+    );
+  }
   return (
-    <div className="container mx-auto p-4">
-      <h1>Rick and Morty</h1>
-      <p>Bem-vindo à aplicação Rick and Morty!</p>
-    </div>
+    <ContentGrid
+      items={randomCharacters}
+      keyExtractor={(c) => c.id}
+      renderItem={(character) => (
+        <Link
+          to={`/characters/${character.id}`}
+          className="block w-full min-w-0"
+        >
+          <CharacterCard character={character} />
+        </Link>
+      )}
+    />
   );
 }
