c1ce0f57243eb9bd4546e96e5fd7e27e94f3cf7a
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-02-14 23:56:35 -0300

    feat: add location retrieval by ID to LocationsService

--- src/services/locations.service.ts
+++ src/services/locations.service.ts
@@ -1,10 +1,15 @@
-import type { LocationResponse } from "@/types/location.ts";
+import type { LocationResponse, Location } from "@/types/location.ts";
 import { API_URL } from "./api.ts";
 
 export const LocationsService = {
   getAll: async (page: number): Promise<LocationResponse> => {
     const response = await fetch(`${API_URL}/location?page=${page}`);
     if (!response.ok) throw new Error("Failed to fetch locations");
+    return response.json();
+  },
+  getById: async (id: number): Promise<Location> => {
+    const response = await fetch(`${API_URL}/location/${id}`);
+    if (!response.ok) throw new Error("Failed to fetch location");
     return response.json();
   },
 };
