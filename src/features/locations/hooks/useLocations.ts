15b7ac7c6dd1d34236f07e5dbf0805fc20f25c54
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-02-17 22:13:41 -0300

    feat: update useLocations hook to accept filters for enhanced location querying

--- src/features/locations/hooks/useLocations.ts
+++ src/features/locations/hooks/useLocations.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { LocationsService } from "@/services/locations.service.ts";
+import type { LocationFilter } from "@/types/filter";
 
-export const useLocations = (page: number) => {
+export const useLocations = (page: number, filters: LocationFilter) => {
   return useQuery({
-    queryKey: ["locations", page],
-    queryFn: () => LocationsService.getAll(page),
+    queryKey: ["locations", page, filters],
+    queryFn: () => LocationsService.getAll(page, filters),
     staleTime: 1000 * 60 * 5,
   });
 };
