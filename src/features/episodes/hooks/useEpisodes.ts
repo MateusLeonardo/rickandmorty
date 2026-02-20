027aecf836999817f20b381180469068f3e30a76
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-02-19 21:22:21 -0300

    feat: update useEpisodes hook to accept filters for enhanced episode querying

--- src/features/episodes/hooks/useEpisodes.ts
+++ src/features/episodes/hooks/useEpisodes.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { EpisodesService } from "@/services/episodes.service.ts";
+import type { EpisodeFilter } from "@/types/filter";
 
-export const useEpisodes = (page: number) => {
+export const useEpisodes = (page: number, filters: EpisodeFilter) => {
   return useQuery({
-    queryKey: ["episodes", page],
-    queryFn: () => EpisodesService.getAll(page),
+    queryKey: ["episodes", page, filters],
+    queryFn: () => EpisodesService.getAll(page, filters),
     staleTime: 1000 * 60 * 5,
   });
 };
