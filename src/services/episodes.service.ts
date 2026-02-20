af1e372b0628660caacaa497a5f266b0a71cf16b
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-02-19 21:22:10 -0300

    feat: enhance getAll method to support episode filtering functionality

--- src/services/episodes.service.ts
+++ src/services/episodes.service.ts
@@ -1,9 +1,18 @@
 import type { Episode, EpisodeResponse } from "@/types/episode.ts";
-import { API_URL } from "./api.ts";
+import { API_URL, toQueryParams } from "./api.ts";
+import type { EpisodeFilter } from "@/types/filter.ts";
 
 export const EpisodesService = {
-  getAll: async (page: number): Promise<EpisodeResponse> => {
-    const response = await fetch(`${API_URL}/episode?page=${page}`);
+  getAll: async (
+    page: number,
+    filters?: EpisodeFilter,
+  ): Promise<EpisodeResponse> => {
+    const params = new URLSearchParams({
+      page: String(page),
+      ...toQueryParams(filters ?? {}),
+    });
+
+    const response = await fetch(`${API_URL}/episode?${params}`);
     if (!response.ok) throw new Error("Failed to fetch episodes");
     return response.json();
   },
