fe938ef18ed0f97ad5e0a0d28b81639b69ee97e1
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2026-02-20 21:39:56 -0300

    feat: add filtering inputs to LocationsPage for enhanced location querying

--- src/features/locations/pages/LocationsPage.tsx
+++ src/features/locations/pages/LocationsPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocations } from "../hooks/useLocations.ts";
 import { PageHeader } from "@/components/layout/PageHeader/index.ts";
 import {
@@ -10,20 +10,58 @@ import {
   LocationCardSkeleton,
 } from "@/components/cards/LocationCard/index.ts";
 import { Pagination } from "@/components/ui/Pagination/index.ts";
+import { FilterInput } from "@/components/ui/Filter/FilterInput.tsx";
+import type { LocationFilter } from "@/types/filter.ts";
+import { useDebounce } from "@/custom-hooks/useDebounce.ts";
 
 const locationGridClassName =
   "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4";
 
 export function LocationsPage() {
   const [page, setPage] = useState(1);
-  const { data: locations, isLoading } = useLocations(page);
+  const [filters, setFilters] = useState<LocationFilter>({
+    name: "",
+    dimension: "",
+    type: "",
+  });
+  const debouncedFilters = useDebounce(filters, 500);
+  const { data: locations, isLoading } = useLocations(page, debouncedFilters);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
+  const handleFilterChange = (key: keyof LocationFilter) => (value: string) => {
+    setFilters((prev) => ({ ...prev, [key]: value }));
+    setPage(1);
+  };
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+  }, []);
   return (
-    <div className="container mx-auto p-4 ">
-      <PageHeader
-        title="Locations"
-        subtitle="Explore the locations of the Rick and Morty series"
-      />
+    <div className="p-4 lg:p-2">
+      <div className="flex justify-between items-center">
+        <PageHeader
+          title="Locations"
+          subtitle="Explore the locations of the Rick and Morty series"
+        />
+
+        <div className="flex items-center gap-4">
+          <FilterInput
+            onChange={handleFilterChange("name")}
+            placeholder="Search"
+            value={filters.name}
+            inputRef={inputRef}
+          />
+          <FilterInput
+            onChange={handleFilterChange("type")}
+            placeholder="Type"
+            value={filters.type}
+          />
+          <FilterInput
+            onChange={handleFilterChange("dimension")}
+            placeholder="Dimension"
+            value={filters.dimension}
+          />
+        </div>
+      </div>
 
       {isLoading ? (
         <ContentGridSkeleton
